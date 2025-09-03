89800b9e135436730fbc2e611df660a7bcab9bbd
ShreyKumra <kumrashrey2006@gmail.com>
2025-09-03 19:15:24 +0530

    Update tut2q3.cpp

--- Lab_assignment_2/tut2q3.cpp
+++ Lab_assignment_2/tut2q3.cpp
@@ -14,37 +14,11 @@ int main(){
         cout<<"enter the element of the array : ";
         cin>>arr[i];
     }
-    int s = 0 ;
-    int diff = arr[1]-arr[0];
-    for (int i = 0; i < n-1; i++)
-    {
-        int t = arr[i+1]-arr[i];
-        if (diff==t)
-        {
+    for(int i=0;i<n-1;i++){
+        if(arr[i+1]-arr[i]==1){
             continue;
         }
-        else{
-            int a = arr[i+1]-arr[i+2];
-            if (a!=diff)
-            {
-                s=i+1;
-            }
-            else{
-                s=i;
-            }  
-            break;  
-        }
-    }
-    cout<<"Missing Element Index : "<<s<<endl;
-    for (int i = n-1; i >= s ; i--)
-    {
-        arr[i+1]=arr[i];
-    }
-    arr[s]=arr[s-1]+diff;
-
-    for (int i = 0; i < n; i++)
-    {
-        cout<<arr[i]<<" ";
+        else cout<<"the missing element is "<<i+2<<endl;
     }
     
 
